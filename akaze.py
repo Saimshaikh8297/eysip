3187a30b6ef79dc74c0214db7127e69481900628
Saim <saimshaikh8297@gmail.com>
2018-06-01 13:09:56 +0530

    contourfilterimages

--- akaze.py
+++ akaze.py
@@ -2,8 +2,8 @@ import cv2
 import numpy as np
 def kaze_match():
     # load the image and convert it to grayscale
-    im1 = cv2.imread("plotlkt.jpg")
-    im2 = cv2.imread("plotlkt.jpg")
+    im1 = cv2.imread("sample6.jpg")
+    im2 = cv2.imread("sample4.jpg")
     gray1 = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
     gray2 = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
 
@@ -12,12 +12,16 @@ def kaze_match():
     sift = cv2.xfeatures2d.SIFT_create()
     surf = cv2.xfeatures2d.SURF_create()
     akaze = cv2.AKAZE_create()
+    brisk = cv2.BRISK_create()
+
     (akazekps1, akazedescs1) = akaze.detectAndCompute(gray1, None)
     (akazekps2, akazedescs2) = akaze.detectAndCompute(gray2, None)
     (siftkps1, siftdescs1) = sift.detectAndCompute(gray1, None)
     (siftkps2, siftdescs2) = sift.detectAndCompute(gray2, None)
     (surfkps1, surfdescs1) = surf.detectAndCompute(gray1, None)
     (surfkps2, surfdescs2) = surf.detectAndCompute(gray2, None)
+    (briskkps1, briskdescs1) = brisk.detectAndCompute(gray1, None)
+    (briskkps2, briskdescs2) = brisk.detectAndCompute(gray2, None)
 
     print("No of KeyPoints:")
     print("akazekeypoints1: {}, akazedescriptors1: {}".format(len(akazekps1), akazedescs1.shape))
@@ -26,6 +30,8 @@ def kaze_match():
     print("siftkeypoints2: {}, siftdescriptors2: {}".format(len(siftkps2), siftdescs2.shape))
     print("surfkeypoints1: {}, surfdescriptors1: {}".format(len(surfkps1), surfdescs1.shape))
     print("surfkeypoints2: {}, surfdescriptors2: {}".format(len(surfkps2), surfdescs2.shape))
+    print("briskkeypoints1: {}, briskdescriptors1: {}".format(len(briskkps1), briskdescs1.shape))
+    print("briskkeypoints2: {}, briskdescriptors2: {}".format(len(briskkps2), briskdescs2.shape))
 
     # Match the features
     bfakaze = cv2.BFMatcher(cv2.NORM_HAMMING)
@@ -33,6 +39,8 @@ def kaze_match():
     akazematches = bfakaze.knnMatch(akazedescs1,akazedescs2, k=2)
     siftmatches = bf.knnMatch(siftdescs1, siftdescs2, k=2)
     surfmatches = bf.knnMatch(surfdescs1, surfdescs2, k=2)
+    briskmatches =bf.knnMatch(briskdescs1, briskdescs2, k=2)
+
 
     # Apply ratio test on AKAZE matches
     goodakaze = []
@@ -40,7 +48,6 @@ def kaze_match():
         if m.distance < 0.9*n.distance:
             goodakaze.append([m])
 
-    # cv2.drawMatchesKnn expects list of lists as matches.
     im3akaze = cv2.drawMatchesKnn(im1, akazekps1, im2, akazekps2, goodakaze[1:20], None, flags=2)
     cv2.imshow("AKAZE matching", im3akaze)
     goodakaze=np.asarray(goodakaze)
@@ -52,7 +59,6 @@ def kaze_match():
         if m.distance < 0.9 * n.distance:
             goodsift.append([m])
 
-    # cv2.drawMatchesKnn expects list of lists as matches.
     im3sift = cv2.drawMatchesKnn(im1, siftkps1, im2, siftkps2, goodsift[1:20], None, flags=2)
     cv2.imshow("SIFT matching", im3sift)
     goodsift = np.asarray(goodsift)
@@ -64,11 +70,21 @@ def kaze_match():
         if m.distance < 0.9 * n.distance:
             goodsurf.append([m])
 
-    # cv2.drawMatchesKnn expects list of lists as matches.
     im3surf = cv2.drawMatchesKnn(im1, surfkps1, im2, surfkps2, goodsurf[1:20], None, flags=2)
     cv2.imshow("SURF matching", im3surf)
     goodsurf = np.asarray(goodsurf)
     print(goodsurf.shape)
+
+    # Apply ratio test on BRISK matches
+    goodbrisk = []
+    for m, n in briskmatches:
+        if m.distance < 0.9 * n.distance:
+            goodbrisk.append([m])
+
+    im3brisk = cv2.drawMatchesKnn(im1, briskkps1, im2, briskkps2, goodbrisk[1:20], None, flags=2)
+    cv2.imshow("BRISK matching", im3brisk)
+    goodbrisk = np.asarray(goodbrisk)
+    print(goodbrisk.shape)
 
     cv2.waitKey(0)
 
